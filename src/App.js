34392f4414dd0a7a45468fa2c1edc2bf61f0652c
Scott van Leeuwen <svanleeuwen@live.com>
2022-06-16 18:22:13 -0400

    delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+function WelcomeBack() {
+  return <p>Welcome back, valued customer!</p>;
+}
+
+function EncouragingWords() {
+  return <p>Believe you can and you’re halfway there.</p>;
+}
+
+function DailyTip() {
+  return <p>Highlight a whole paragraph by triple-clicking on it.</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <WelcomeBack />
+      <WelcomeBack />
+      <EncouragingWords />
+      <DailyTip />
+    </>
   );
 }
-
 export default App;
